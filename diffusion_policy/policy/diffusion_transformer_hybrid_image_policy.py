f8999b6adbc67179d1714f052ff01f96ebcc0599
greensongavin <gavingreenson@icloud.com>
2025-01-27 12:52:16 +0800

    add: monkey patching for diffusion_policy's visual encoder and transformer decoder

--- diffusion_policy/policy/diffusion_transformer_hybrid_image_policy.py
+++ diffusion_policy/policy/diffusion_transformer_hybrid_image_policy.py
@@ -5,6 +5,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange, reduce
 from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
+import builtins, types, inspect
+from omegaconf import DictConfig
+import pathlib
+import dill
+import copy
+import hydra
 
 from diffusion_policy.model.common.normalizer import LinearNormalizer
 from diffusion_policy.policy.base_image_policy import BaseImagePolicy
@@ -15,6 +21,9 @@ from robomimic.algo import algo_factory
 from robomimic.algo.algo import PolicyAlgo
 import robomimic.utils.obs_utils as ObsUtils
 import robomimic.models.base_nets as rmbn
+from robomimic.models.obs_nets import ObservationEncoder
+from robomimic.models.base_nets import VisualCore
+from robomimic.models.base_nets import ResNet18Conv, SpatialSoftmax
 import diffusion_policy.model.vision.crop_randomizer as dmvc
 from diffusion_policy.common.pytorch_util import dict_apply, replace_submodules
 
@@ -106,7 +115,7 @@ class DiffusionTransformerHybridImagePolicy(BaseImagePolicy):
                 device='cpu',
             )
 
-        obs_encoder = policy.nets['policy'].nets['encoder'].nets['obs']
+        obs_encoder: ObservationEncoder = policy.nets['policy'].nets['encoder'].nets['obs']
         
         if obs_encoder_group_norm:
             # replace batch norm with group norm
@@ -371,6 +380,281 @@ class DiffusionTransformerHybridImagePolicy(BaseImagePolicy):
         pred = self.model(noisy_trajectory, timesteps, cond)
 
         pred_type = self.noise_scheduler.config.prediction_type 
+        if pred_type == 'epsilon':
+            target = noise
+        elif pred_type == 'sample':
+            target = trajectory
+        else:
+            raise ValueError(f"Unsupported prediction type {pred_type}")
+
+        loss = F.mse_loss(pred, target, reduction='none')
+        loss = loss * loss_mask.type(loss.dtype)
+        loss = reduce(loss, 'b ... -> b (...)', 'mean')
+        loss = loss.mean()
+        return loss
+
+
+class DiffusionTransformerHybridImagePolicyHDFree(DiffusionTransformerHybridImagePolicy):
+    def __init__(self,
+                 domain_adapt: DictConfig,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.src_ckpt = domain_adapt.src_ckpt
+        self.load_from_src_ckpt(self.src_ckpt)  # will have 2 copies: one is for target; another is for source (fixed)
+
+        # (Source) Create model copies for domain adaptation AFTER loading pretrained weights
+        self.src_obs_encoder = copy.deepcopy(self.obs_encoder)
+        self.src_model = copy.deepcopy(self.model)
+        self.placeholder_param = torch.nn.Parameter(torch.ones([1]))
+
+        # (Loss) For domain adaptation
+        self.use_da_vis1: bool = domain_adapt.use_da_visual in ('both', 'static')
+        self.use_da_vis2: bool = domain_adapt.use_da_visual in ('both', 'gripper')  # Not implemented!
+        self.use_da_act: bool = domain_adapt.use_da_act
+        self.act_loss_from: str = domain_adapt.act_loss_from
+        self.act_layers: int = domain_adapt.act_layers
+        self.act_weights: str = domain_adapt.act_weights
+
+        # (Target) Register Adapter blocks for trainable modules
+        if 'adapter' in domain_adapt.act_weights:
+            pass
+            # self.model.inner_model.decoder.register_adapter()
+        debug_diff_loss = domain_adapt.debug_diff_loss
+        if debug_diff_loss:
+            self.use_da_vis1 = self.use_da_vis2 = False
+        if self.use_da_vis1:
+            self.da_vis1_loss = copy.deepcopy(domain_adapt.visual_da).to(self.device)
+        if self.use_da_vis2:
+            self.da_vis2_loss = copy.deepcopy(domain_adapt.visual_da).to(self.device)
+        if self.use_da_act:
+            self.da_act_loss = copy.deepcopy(domain_adapt.action_da).to(self.device)
+
+        # (Target) Using Monkey Patching trick to replace class method
+        def forward_with_da(self: VisualCore, inputs):
+            """
+            Forward pass through visual core.
+            """
+            ndim = len(self.input_shape)
+            assert tuple(inputs.shape)[-ndim:] == tuple(self.input_shape)
+            x = self.nets[:-2](inputs)
+            # print(x.shape, x.mean(), x.min(), x.max())
+            x = self.nets[-2](x)
+            # print(x.shape, x.mean(), x.min(), x.max())
+            x = self.nets[-1](x)
+            # print(x.shape, x.mean(), x.min(), x.max())
+            if list(self.output_shape(list(inputs.shape)[1:])) != list(x.shape)[1:]:
+                raise ValueError('Size mismatch: expect size %s, but got size %s' % (
+                    str(self.output_shape(list(inputs.shape)[1:])), str(list(x.shape)[1:]))
+                                 )
+            # print("[DEBUG] with_da_forward")
+            return x
+
+        self.obs_encoder.obs_nets['image'].forward = types.MethodType(
+            forward_with_da, self.obs_encoder.obs_nets['image'])
+
+    def load_from_src_ckpt(self, src_ckpt, use_ema: bool = True):
+        self.src_ckpt = src_ckpt
+        path = pathlib.Path(src_ckpt)
+        payload = torch.load(path.open('rb'), pickle_module=dill)
+
+        # print(payload['state_dicts'].keys())
+        if use_ema:
+            self.load_state_dict(payload['state_dicts']['ema_model'])
+        else:
+            self.load_state_dict(payload['state_dicts']['model'])
+        print(f"[DiffusionTransformerHybridImagePolicyHDFree] Loaded src_ckpt (use_ema={use_ema}) "
+              f"from: {self.src_ckpt}")
+
+    # ========= inference  ============
+    def conditional_sample(self,
+                           condition_data, condition_mask,
+                           cond=None, generator=None,
+                           # keyword arguments to scheduler.step
+                           **kwargs
+                           ):
+        model = self.model
+        scheduler = self.noise_scheduler
+
+        trajectory = torch.randn(
+            size=condition_data.shape,
+            dtype=condition_data.dtype,
+            device=condition_data.device,
+            generator=generator)
+
+        # set step values
+        scheduler.set_timesteps(self.num_inference_steps)
+
+        for t in scheduler.timesteps:
+            # 1. apply conditioning
+            trajectory[condition_mask] = condition_data[condition_mask]
+
+            # 2. predict model output
+            model_output = model(trajectory, t, cond)
+
+            # 3. compute previous image: x_t -> x_t-1
+            trajectory = scheduler.step(
+                model_output, t, trajectory,
+                generator=generator,
+                **kwargs
+            ).prev_sample
+
+        # finally make sure conditioning is enforced
+        trajectory[condition_mask] = condition_data[condition_mask]
+
+        return trajectory
+
+    def predict_action(self, obs_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """
+        obs_dict: must include "obs" key
+        result: must include "action" key
+        """
+        assert 'past_action' not in obs_dict  # not implemented yet
+        # normalize input
+        nobs = self.normalizer.normalize(obs_dict)
+        value = next(iter(nobs.values()))
+        B, To = value.shape[:2]
+        T = self.horizon
+        Da = self.action_dim
+        Do = self.obs_feature_dim
+        To = self.n_obs_steps
+
+        # build input
+        device = self.device
+        dtype = self.dtype
+
+        # handle different ways of passing observation
+        cond = None
+        cond_data = None
+        cond_mask = None
+        if self.obs_as_cond:
+            this_nobs = dict_apply(nobs, lambda x: x[:, :To, ...].reshape(-1, *x.shape[2:]))
+            nobs_features = self.obs_encoder(this_nobs)
+            # reshape back to B, To, Do
+            cond = nobs_features.reshape(B, To, -1)
+            shape = (B, T, Da)
+            if self.pred_action_steps_only:
+                shape = (B, self.n_action_steps, Da)
+            cond_data = torch.zeros(size=shape, device=device, dtype=dtype)
+            cond_mask = torch.zeros_like(cond_data, dtype=torch.bool)
+        else:
+            # condition through impainting
+            this_nobs = dict_apply(nobs, lambda x: x[:, :To, ...].reshape(-1, *x.shape[2:]))
+            nobs_features = self.obs_encoder(this_nobs)
+            # reshape back to B, To, Do
+            nobs_features = nobs_features.reshape(B, To, -1)
+            shape = (B, T, Da + Do)
+            cond_data = torch.zeros(size=shape, device=device, dtype=dtype)
+            cond_mask = torch.zeros_like(cond_data, dtype=torch.bool)
+            cond_data[:, :To, Da:] = nobs_features
+            cond_mask[:, :To, Da:] = True
+
+        # run sampling
+        nsample = self.conditional_sample(
+            cond_data,
+            cond_mask,
+            cond=cond,
+            **self.kwargs)
+
+        # unnormalize prediction
+        naction_pred = nsample[..., :Da]
+        action_pred = self.normalizer['action'].unnormalize(naction_pred)
+
+        # get action
+        if self.pred_action_steps_only:
+            action = action_pred
+        else:
+            start = To - 1
+            end = start + self.n_action_steps
+            action = action_pred[:, start:end]
+
+        result = {
+            'action': action,
+            'action_pred': action_pred
+        }
+        return result
+
+    # ========= training  ============
+    def set_normalizer(self, normalizer: LinearNormalizer):
+        self.normalizer.load_state_dict(normalizer.state_dict())
+
+    def get_optimizer(
+            self,
+            transformer_weight_decay: float,
+            obs_encoder_weight_decay: float,
+            learning_rate: float,
+            betas: Tuple[float, float]
+    ) -> torch.optim.Optimizer:
+        optim_groups = self.model.get_optim_groups(
+            weight_decay=transformer_weight_decay)
+        optim_groups.append({
+            "params": self.obs_encoder.parameters(),
+            "weight_decay": obs_encoder_weight_decay
+        })
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=learning_rate, betas=betas
+        )
+        return optimizer
+
+    def compute_loss(self, batch):
+        # normalize input
+        assert 'valid_mask' not in batch
+        nobs = self.normalizer.normalize(batch['obs'])
+        nactions = self.normalizer['action'].normalize(batch['action'])
+        batch_size = nactions.shape[0]
+        horizon = nactions.shape[1]
+        To = self.n_obs_steps
+
+        # handle different ways of passing observation
+        cond = None
+        trajectory = nactions
+        if self.obs_as_cond:
+            # reshape B, T, ... to B*T
+            this_nobs = dict_apply(nobs,
+                                   lambda x: x[:, :To, ...].reshape(-1, *x.shape[2:]))
+            nobs_features = self.obs_encoder(this_nobs)
+            # reshape back to B, T, Do
+            cond = nobs_features.reshape(batch_size, To, -1)
+            if self.pred_action_steps_only:
+                start = To - 1
+                end = start + self.n_action_steps
+                trajectory = nactions[:, start:end]
+        else:
+            # reshape B, T, ... to B*T
+            this_nobs = dict_apply(nobs, lambda x: x.reshape(-1, *x.shape[2:]))
+            nobs_features = self.obs_encoder(this_nobs)
+            # reshape back to B, T, Do
+            nobs_features = nobs_features.reshape(batch_size, horizon, -1)
+            trajectory = torch.cat([nactions, nobs_features], dim=-1).detach()
+
+        # generate impainting mask
+        if self.pred_action_steps_only:
+            condition_mask = torch.zeros_like(trajectory, dtype=torch.bool)
+        else:
+            condition_mask = self.mask_generator(trajectory.shape)
+
+        # Sample noise that we'll add to the images
+        noise = torch.randn(trajectory.shape, device=trajectory.device)
+        bsz = trajectory.shape[0]
+        # Sample a random timestep for each image
+        timesteps = torch.randint(
+            0, self.noise_scheduler.config.num_train_timesteps,
+            (bsz,), device=trajectory.device
+        ).long()
+        # Add noise to the clean images according to the noise magnitude at each timestep
+        # (this is the forward diffusion process)
+        noisy_trajectory = self.noise_scheduler.add_noise(
+            trajectory, noise, timesteps)
+
+        # compute loss mask
+        loss_mask = ~condition_mask
+
+        # apply conditioning
+        noisy_trajectory[condition_mask] = trajectory[condition_mask]
+
+        # Predict the noise residual
+        pred = self.model(noisy_trajectory, timesteps, cond)
+
+        pred_type = self.noise_scheduler.config.prediction_type
         if pred_type == 'epsilon':
             target = noise
         elif pred_type == 'sample':
